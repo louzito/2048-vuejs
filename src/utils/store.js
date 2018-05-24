412722cddfcbc1050055ee66480960e52e4b9c8c
Axel Prouin <axelprouin@gmail.com>
2018-05-24 17:03:44 +0200

    rebase dev

--- src/utils/store.js
+++ src/utils/store.js
@@ -6,15 +6,22 @@ Vue.use(Vuex)
 const store = new Vuex.Store({
     state: {
         board: null,
+        nickname: null
     },
     getters: {
         getBoard(state) {
-            return state.board
+          return state.board
+        },
+        getNickname(state) {
+          return state.nickname
         }
     },
     mutations: {
         setBoard(state, board) {
             state.board = board
+        },
+        setNickname(state, nickname) {
+            state.nickname = nickname
         }
     }
 })
