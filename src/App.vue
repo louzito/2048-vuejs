e140a94196e47a4f348446e8444e6f1a2395e73c
Lou Zito <zito.lou@gmail.com>
2018-05-24 15:11:34 +0200

    jeu fonctionnel

--- src/App.vue
+++ src/App.vue
@@ -31,4 +31,20 @@ li {
   display: inline-block;
   margin: 0 10px;
 }
+.jeu {text-align: center;}
+#board-game {display: inline-block;padding: 30px;margin: 20px auto;background:#000000}
+#board-game .board-line {font-size: 0;display:inline-block;vertical-align: top;}
+#board-game .board-tile {display: block;width: 100px;height: 100px;line-height: 100px;font-size: 50px;color: #000;font-weight: bold;background: #FFF;border: 1px solid lightgrey;border-radius: 5px;text-align: center;margin: 2px;}
+#board-game .board-tile.bt2 {background: #E1E6FA;}
+#board-game .board-tile.bt4 {background: #C4D7ED;}
+#board-game .board-tile.bt8 {background: #ABC8E2;}
+#board-game .board-tile.bt16 {background: #96B0C7;}
+#board-game .board-tile.bt32 {background: #60A2E0;}
+#board-game .board-tile.bt64 {background: #528ABF;}
+#board-game .board-tile.bt128 {background: #4D83B5;}
+#board-game .board-tile.bt256 {background: #375D81;color: #FFF;}
+#board-game .board-tile.bt512 {background: #2A4863;color: #FFF;}
+#board-game .board-tile.bt1024 {background: #183152;color: #FFF;}
+#board-game .board-tile.bt2048 {background: #0F1E33;color: #FFF;}
+#board-game .board-tile.bt4096 {background: #08111C;color: #FFF;}
 </style>
